6078fb6a6343a276b869a9421b1d5938d51b533b
iqDF <daniel.thekurniadi@gmail.com>
2020-05-14 22:35:22 +0800

    Implement url mapping handler - implement by reading yaml - implement from hardcoded mapping

--- handler.go
+++ handler.go
@@ -1,8 +1,16 @@
 package urlshort
 
 import (
+	yaml "gopkg.in/yaml.v2"
 	"net/http"
 )
+
+// YAMLPathURL represent YAML file which
+// maps a URI Path to another valid URL
+type YAMLPathURL struct {
+	URL  string `yaml:"url"`
+	Path string `yaml:"path"`
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -11,8 +19,15 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if url, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, url, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+	return handler
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -32,6 +47,33 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	var handler func(http.ResponseWriter, *http.Request)
+
+	yamlPathURLs, err := parseYAML(yml)
+	if err != nil {
+		return handler, err
+	}
+
+	pathsToUrls := makeYAMLToMap(yamlPathURLs)
+	handler = MapHandler(pathsToUrls, fallback)
+	return handler, err
+}
+
+// parseYAML take YAML file content in []byte (array) and map the content
+// into a YAMLPathURL struct above
+func parseYAML(yml []byte) ([]YAMLPathURL, error) {
+	var yamlPathURLs []YAMLPathURL
+	err := yaml.Unmarshal(yml, &yamlPathURLs)
+
+	return yamlPathURLs, err
+}
+
+// makeYAMLToMap convert array of struct, []YAMLPathURL, into
+// native map of Path to URLs
+func makeYAMLToMap(yamlPathURLs []YAMLPathURL) map[string]string {
+	var pathsToUrls = make(map[string]string)
+	for _, yamlPathURL := range yamlPathURLs {
+		pathsToUrls[yamlPathURL.Path] = yamlPathURL.URL
+	}
+	return pathsToUrls
 }
